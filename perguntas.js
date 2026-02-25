0e392320364429857d22d43a99fa33f8426c62b7
Thayne V. <santos.thayne@escola.pr.gov.br>
2026-02-25 11:42:55 -0300

    1

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+
+
+criaCartao(
+    'TERMOQUÍMICA',
+    'O que é entalpia?',
+    '- Grandeza termodinâmica que mede o conteúdo de calor de um sistema, especialmente em reações químicas a pressão constante.'
+)
+
+criaCartao(
+    'REAÇÕES QUÍMICAS',
+    'O que é uma reação exotérmica?',
+    '- Uma reação exotérmica é aquela que liberta calor para o ambiente, resultando em um aumento da temperatura do sistema.'
+)
+
+criaCartao(
+    'CONCEITOS',
+    'O que diz a Lei de Hess?',
+    '- Estabelece que a variação de entalpia (calor da reação) de uma reação química depende apenas do estado inicial e final dos reagentes e produtos, e não do caminho ou etapas intermediárias que a reação possa seguir.'
+)
+
+criaCartao(
+    'TIPOS DE ENTALPIA',
+    'Quais são os dois tipos de entalpia mais conhecidos?',
+    '- Entalpia de Formação e Entalpia de Ligação.'
+)
